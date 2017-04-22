4677ff3ca26742402b8866904e8260f6d95ec408
Jason Reed <jcreed@gmail.com>
2017-04-22 16:08:19 -0400

    factor audio out

---
+++ audio.ts
@@ -0,0 +1,71 @@
+import { Score, Note } from './types';
+import * as _ from "lodash";
+
+export const ad = new AudioContext();
+const RATE = ad.sampleRate; // most likely 44100, maybe 48000?
+
+function freq_of_pitch(pitch) {
+  return 440 * Math.pow(2, (pitch - 69) / 12);
+}
+
+const global_adsr_params = {a: 0.01, d: 0.1, s: 0.5, r: 0.01};
+
+function adsr(params, length) {
+  const {a, d, s, r} = params;
+  return {
+	 env_f: (t => {
+		if (t < a) { return t / a }
+		if (t - a < d) { const T = (t - a) / d; return s * T + (1 - T); }
+		if (t < length) return s;
+		return s * (1 - (t - length) / r);
+	 }),
+	 real_length: length + r
+  }
+}
+
+
+function audio_render_notes(ad, score: Score, progress: (t: number) => void) {
+  const segmentTime = score.duration * score.seconds_per_tick;
+  const len = segmentTime * RATE; // in frames
+  var buf = ad.createBuffer(1, len, RATE);
+  var dat = buf.getChannelData(0);
+  _.each(score.patterns, (pat: Note[], patName: string) => {
+	 pat.forEach(note => {
+		const {env_f, real_length} = adsr(global_adsr_params,
+													 (note.time[1] - note.time[0]) * score.seconds_per_tick);
+		const note_start_frame = Math.round(note.time[0] * score.seconds_per_tick * RATE);
+		const note_term_frame = Math.round(note.time[0] * score.seconds_per_tick * RATE + real_length * RATE);
+		const state = {phase: 0, amp: 1, f: freq_of_pitch(note.pitch)};
+
+		for (var t = note_start_frame; t < note_term_frame; t++) {
+		  state.phase += state.f / RATE;
+		  let e = env_f((t - note_start_frame) / RATE);
+		  const wav = 0.15 * Math.sin(3.1415926535 * 2 * state.phase);
+		  //		const wav = 0.05 * ((state.phase - Math.floor(state.phase) < 0.5) ? 1 : -1);
+		  dat[t] += e * state.amp * wav ;
+		}
+	 });
+  });
+
+  var src = ad.createBufferSource();
+
+  const beginTime = ad.currentTime + 0.1;
+
+  const ival = setInterval(() => {
+	 if (ad.currentTime < beginTime)
+		return;
+	 progress((ad.currentTime - beginTime) / score.seconds_per_tick);
+  }, 40);
+
+  src.onended = () => {
+	 progress(null);
+	 clearInterval(ival);
+  }
+  src.buffer = buf;
+  src.connect(ad.destination);
+  src.start(beginTime);
+}
+
+export function play(score, progress) {
+  audio_render_notes(ad, score, progress);
+}
--- main.ts
+++ main.ts
@@ -5,83 +5,13 @@ import { MouseState, MouseAction, Action, AppState, Note, mpoint, Mode, Score,
 			initialState } from './types';
 import { keyOf } from './key';
 import { Immutable as Im, get, set, update, getIn, setIn, updateIn, fromJS, toJS } from './immutable';
-
+import { play } from './audio';
 import * as _ from "lodash";
-
-export const ad = new AudioContext();
-const RATE = ad.sampleRate; // most likely 44100, maybe 48000?
 
 declare const debug_glob: any;
 
 const notes = score.patterns['default'];
 notes.forEach(note => note.pitch += 12);
-
-function freq_of_pitch(pitch) {
-  return 440 * Math.pow(2, (pitch - 69) / 12);
-}
-
-const global_adsr_params = {a: 0.01, d: 0.1, s: 0.5, r: 0.01};
-
-function adsr(params, length) {
-  const {a, d, s, r} = params;
-  return {
-	 env_f: (t => {
-		if (t < a) { return t / a }
-		if (t - a < d) { const T = (t - a) / d; return s * T + (1 - T); }
-		if (t < length) return s;
-		return s * (1 - (t - length) / r);
-	 }),
-	 real_length: length + r
-  }
-}
-
-
-function audio_render_notes(ad, score: Score) {
-  const segmentTime = score.duration * score.seconds_per_tick;
-  const len = segmentTime * RATE; // in frames
-  var buf = ad.createBuffer(1, len, RATE);
-  var dat = buf.getChannelData(0);
-  _.each(score.patterns, (pat: Note[], patName: string) => {
-	 pat.forEach(note => {
-		const {env_f, real_length} = adsr(global_adsr_params,
-													 (note.time[1] - note.time[0]) * score.seconds_per_tick);
-		const note_start_frame = Math.round(note.time[0] * score.seconds_per_tick * RATE);
-		const note_term_frame = Math.round(note.time[0] * score.seconds_per_tick * RATE + real_length * RATE);
-		const state = {phase: 0, amp: 1, f: freq_of_pitch(note.pitch)};
-
-		for (var t = note_start_frame; t < note_term_frame; t++) {
-		  state.phase += state.f / RATE;
-		  let e = env_f((t - note_start_frame) / RATE);
-		  const wav = 0.15 * Math.sin(3.1415926535 * 2 * state.phase);
-		  //		const wav = 0.05 * ((state.phase - Math.floor(state.phase) < 0.5) ? 1 : -1);
-		  dat[t] += e * state.amp * wav ;
-		}
-	 });
-  });
-
-  var src = ad.createBufferSource();
-
-  const beginTime = ad.currentTime + 0.1;
-
-  const ival = setInterval(() => {
-	 if (ad.currentTime < beginTime)
-		return;
-	 dispatch({t: "SetCurrentPlaybackTime",
-				  v: (ad.currentTime - beginTime) / score.seconds_per_tick});
-  }, 40);
-
-  src.onended = () => {
-	 dispatch({t: "SetCurrentPlaybackTime", v: null});
-	 clearInterval(ival);
-  }
-  src.buffer = buf;
-  src.connect(ad.destination);
-  src.start(beginTime);
-}
-
-function play(score) {
-  audio_render_notes(ad, score);
-}
 
 window.onload = () => {
   document.onkeydown = (e) => dispatch({t: "Key", key: keyOf(e)});
@@ -307,7 +237,7 @@ export function dispatch(a: Action) {
 		render();
 	 break;
   case "Play":
-	 play(a.score);
+	 play(a.score, v => dispatch({t: "SetCurrentPlaybackTime", v}));
 	 break;
   case "SetCurrentPlaybackTime":
 	 state = set(state, 'offsetTicks', a.v);
