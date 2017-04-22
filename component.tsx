069a4544597eb7c9be88805f45f637bf5baaaebf
Jason Reed <jcreed@gmail.com>
2017-04-22 15:30:41 -0400

    show current pattern name

--- component.tsx
+++ component.tsx
@@ -5,6 +5,15 @@ import { dispatch, unreachable } from './main';
 import { AppState, Mode } from './types';
 import * as CSSTransitionGroup from 'preact-css-transition-group';
 
+function ModeHeader({mode}:{mode: Mode}): JSX.Element {
+  switch (mode.t) {
+	 case "editPattern":
+		return <div className="modeHeader">{mode.patName}</div>;
+	 default:
+		return <div className="modeHeader">???</div>;
+  }
+}
+
 function ModeComponent({props}:{props: AppState}): JSX.Element {
   const mode: Mode = props.mode;
   switch (mode.t) {
@@ -28,7 +37,10 @@ export function component_render(props: AppState) {
   const cont = document.getElementById('canvas_container');
   const playClick = () => dispatch({t: "Play", score: props.score});
 
-  const cc = <div>
+  const cc =
+  <div>
+	 <ModeHeader mode={props.mode} />
+	 <div className="workspace">
 		<button onClick={playClick}>Play</button><br/>
 		<ModeComponent props={props} />
 		<div>
@@ -40,6 +52,7 @@ export function component_render(props: AppState) {
 			 </CSSTransitionGroup>
 		  </div>
 		</div>
+	 </div>
   </div>;
   render(cc, cont, cont.lastElementChild);
 }
